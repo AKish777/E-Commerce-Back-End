848272162d956b9b8e6bf2faecb5419369f7155f
Alex Kichigin <alexkichigin@Alexs-MacBook-Pro.local>
2021-09-22 19:54:46 -0400

    pushing proper javascript file

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+
 const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+});
+
+app.use(require("./Routes/api.js"));
+app.use(require("./Routes/html.js"))
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
