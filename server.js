50e6cbf8e901fa4d71e00627571ae84cee4da779
Alex Kichigin <alexkichigin@Alexs-MacBook-Pro.local>
2021-09-22 23:58:10 -0400

    fixing code

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-    process.env.ATLAS_URI || "mongodb://localhost/fitness-tracker-akish777", 
+    process.env.MONGODB_URI || "mongodb://localhost/fitness-tracker-akish777", 
     {
         useNewUrlParser: true,
         useFindAndModify: false,
