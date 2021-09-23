e97376ec265f135bc8e068e392a9e94b2a327d43
Alex Kichigin <alexkichigin@Alexs-MacBook-Pro.local>
2021-09-22 23:52:55 -0400

    fixing code

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,7 +1,7 @@
 let mongoose = require("mongoose");
 let db = require("../Models/workout");
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.ATLAS_URI, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
